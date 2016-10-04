6f90cdfc763ed9c90f1383f26a48be3b57c2d623
Mateusz Szymański <hello`at`mateuszszymanski`dot`eu (remove ` from address, at === @, dot === .)>
2016-10-05 00:57:16 +0200

    reset css

--- app/components/webpage.js
+++ app/components/webpage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import HeaderComponent from './Header/HeaderComponent';
+import './reset.css';
 
 class Webpage extends React.Component {
   render() {
     return (
-      <h1>Hello World</h1>
+      <HeaderComponent />
     )
   }
 }
